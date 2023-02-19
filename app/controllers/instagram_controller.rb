7c30e0b432fcaf05e64c2a5a5018585ec9a2e022
Thomas <ming100628@gmail.com>
2023-02-19 10:00:50 +0800

    navigation bar

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -3,7 +3,9 @@ class InstagramController < ApplicationController
   before_action :ensure_login, except: [:login_page]
   def index; end
 
-  def login_page; end
+  def login_page
+    redirect_to '/instagram' if current_user
+  end
 
   def ensure_login
     redirect_to '/instagram/login_page' if current_user.nil?
