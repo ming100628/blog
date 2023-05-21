f78018c93521540ab5a934cec3c2db8b18dacb4d
Thomas <ming100628@gmail.com>
2023-05-21 09:18:35 +0800

    Filter messages json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
-  layout "instagram_layout"
-  def index; end
+  layout 'instagram_layout'
+  def index
+    respond_to do |format|
+      format.html do
+      end
+      format.json do
+        render json: Message.where(sender_id: current_user.id).or(Message.where(receiver_id: current_user.id)).order(created_at: :asc),
+               status: 200
+      end
+    end
+  end
 end
