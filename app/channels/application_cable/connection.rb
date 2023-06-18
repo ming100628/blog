efc5135e6a82b1649e140cc8cc4869f78acdb6f9
Thomas <ming100628@gmail.com>
2023-06-18 11:21:18 +0800

    web socket

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = User.find_by(token: cookies['secret'])
+    end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,10 @@ class Message < ApplicationRecord
   belongs_to :receiver, class_name: 'User'
 
   validates :content, presence: true
+
+  after_create :broadcast_message
+
+  def broadcast_message
+    ActionCable.server.broadcast("messages:#{receiver.id}", self)
+  end
 end
