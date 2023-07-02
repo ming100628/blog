5b2bd5b753b8c8faae6b164fcbe2c6ac121f2966
Thomas <ming100628@gmail.com>
2023-07-02 10:20:09 +0800

    search users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,29 @@
 class UsersController < ApplicationController
+  def index
+    search_string = params[:search]
+    render json: User.select(:username, :id).where('LOWER(username) LIKE ?', "%#{search_string.downcase}%")
+  end
+
   def signed_in
     user = User.find_by(username: params[:username])
     if user.nil?
       flash[:errors] = ['Username is not found.']
       respond_to do |format|
         format.html do
-            render 'index'
+          render 'index'
         end
         format.json do
-            render json: { message: 'Wrong password!' }, status: 403
+          render json: { message: 'Wrong password!' }, status: 403
         end
       end
     else
       cookies['secret'] = user.token if user.check_password(params[:password])
       respond_to do |format|
         format.html do
-            redirect_to '/'
-
+          redirect_to '/'
         end
         format.json do
-            render json: {  }, status: 200
+          render json: {}, status: 200
         end
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  get '/login', to: 'users#index'
   post '/login', to: 'users#signed_in'
   get '/create_user', to: 'users#new'
   post '/create_user', to: 'users#create'
   get '/logout', to: 'users#logout'
+  get '/users', to: 'users#index'
   get '/dashboard', to: 'dashboard#index'
   get '/all_articles', to: 'articles#a'
   get '/todos', to: 'todos#index'
