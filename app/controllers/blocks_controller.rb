2abf4066d3809a5d9a306ab7cdd762fe12809c97
Thomas <ming100628@gmail.com>
2023-09-17 10:10:36 +0800

    block user

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -2,6 +2,7 @@ class BlocksController < ApplicationController
   def block
     blocker_id = current_user.id
     Block.find_or_create_by(blocker_id:, blocked_id: params[:blocked_id])
+    render json: {}, status: 200
   end
 
   def delete_block
